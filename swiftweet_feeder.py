85746bf3d1df379de8f930494e5e3bfa668c5eb9
pengzhengyi <zhengyi_peng@brown.edu>
2018-10-31 11:55:56 -0400

    README updated about stop

--- swiftweet_feeder.py
+++ swiftweet_feeder.py
@@ -2,6 +2,11 @@ import socketserver
 from http.server import BaseHTTPRequestHandler
 from swiftweet_packer import pack_tweets
 
+#consumer_key: 'CxNW4dbGgBhpPvt0YRn6tOgPg',
+#consumer_secret: 'xnv05OUBTvpwZyw2n8ujWxLiYpRQiMY766paD7ojqXloaqlylt',
+#access_token_key: '4846560701-JDwgLQHKAdZiASRnpwdAHQSx9qzDvSX25JLDcQU',
+#access_token_secret: 'Y6gG4tpZiReGydRHigSWFZTszCW31iKpRxuKTj8whKpAP'
+
 class TweetsFeeder(BaseHTTPRequestHandler):
     def do_GET(self):
         print("<----- Request Start ----->")
@@ -12,6 +17,7 @@ class TweetsFeeder(BaseHTTPRequestHandler):
         if self.path == '/feed/start':
             self.send_response(200)
             self.send_header("Content-type", "application/json")
+            self.send_header("Access-Control-Allow-Origin", "*")
             self.end_headers()
             print("sending a pack of tweets")
             self.wfile.write(pack_tweets().encode())
